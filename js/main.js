4619bb0af1fb403a09f097ce2c1b2fc0923af2fa
Sognus <sognus@sognus.cz>
2023-03-22 15:04:00 +0100

    Buttons to move between scenes, calculation of positions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import * as L from '../vendors/leaflet/leaflet-src.esm.js';
 import * as pannellum from '../vendors/pannellum/pannellum.js';
 
+const PITCH = -15;
+// In FurnaceTaken units
+const MAX_DISTANCE = 500;
+// Delta fucked up, east is north
+const CORRECTION_OFFSET = -62;
+const COMPASS_CORRECTION_OFFSET = CORRECTION_OFFSET * -1;
+
+
 var map;
 var factorX;
 var factorY;
@@ -41,7 +49,7 @@ function createMap() {
 	
 	fetch("../content/data.json")
 		.then((response) => response.json())
-		.then((json) => json.forEach(element => new L.Marker([element.y * factorY, element.x * factorX]).addTo(map).on('click', showPanorama, this).options = element.id));
+		.then((json) => json.forEach(element => new L.Marker([element.y * factorY, element.x * factorX]).addTo(map).on('click', showPanoramaClick, this).options = element.id));
 }
 
 createMap();
@@ -53,12 +61,77 @@ var oPanorama = document.getElementById("panorama");
 //document.querySelector('#panorama .btn_fullscreen').addEventListener('click', toggleFullscreen);
 document.querySelector('#panorama .btn_exit').addEventListener('click', exitPanorama);
 
-function showPanorama(e) {
+async function fetchJSONData() {
+    var response = await fetch("../content/data.json");
+    var data = await response.json();
+    return data;
+}
+
+function getViewInfo(viewID, data) {
+    return data.filter(function(entry) {
+        return entry.id == viewID;
+    })[0];
+}
+
+function customHotspotClickFunction(event, handlerArgs) {
+    event.preventDefault();
+    document.getElementById("panorama").innerHTML = "";
+    showPanorama(handlerArgs.id);
+}
+
+function initHotspots(viewInfo, data) {
+    var hotspots = [];
+
+    data.forEach(element => {
+        if(element.id == viewInfo.id) {
+            return
+        }
+
+        const dx = viewInfo.x - element.x;  // delta x (inches)
+        const dy = viewInfo.y - element.y;  // delta y (inches)
+        const distance = Math.sqrt(dx*dx + dy*dy); // dinstance in inches
+        const yaw = -Math.atan2(dx, dy) * 180 / Math.PI;;  // pitch angle
+        const pitch = Math.atan2(distance, dy) * 180 / Math.PI;  // yaw angle
+        
+        console.log(viewInfo.id + " -> " + element.id + ": "  +distance + "( " + yaw + "  )");
+
+
+        const hotspotObject = new Object();
+        hotspotObject.pitch = PITCH;
+        hotspotObject.yaw = yaw + CORRECTION_OFFSET;
+        hotspotObject.type = "scene";
+        hotspotObject.text = element.id;
+        hotspotObject.sceneId = element.id;
+        hotspotObject.clickHandlerFunc = customHotspotClickFunction;
+        hotspotObject.clickHandlerArgs = element;
+
+        if(distance <= MAX_DISTANCE) {
+            hotspots.push(hotspotObject);
+        }
+
+    });
+
+    return hotspots;
+}
+
+function showPanoramaClick(e) {
+    showPanorama(e.sourceTarget.options);
+}
+
+
+async function showPanorama(viewID) {
+    var data = await fetchJSONData();
+    var viewInfo = getViewInfo(viewID, data);
+    var hotspots = initHotspots(viewInfo, data);
+
 	window.pannellum.viewer('panorama', {
-            "type": "equirectangular",
-            "panorama": "content/views/"+e.sourceTarget.options+".jpg",
-            "autoLoad": true
-        })
+        "type": "equirectangular",
+        "panorama": "content/views/"+viewID+".jpg",
+        "autoLoad": true,
+        "hotSpots": hotspots,
+        "compass": true,
+        "northOffset": COMPASS_CORRECTION_OFFSET
+    })
 	
 	oMap.classList.add("hidden");
 	oPanorama.classList.remove("hidden");
