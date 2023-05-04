cea8311387af9041716054b9849c725eadecec92
Sognus <sognus@sognus.cz>
2023-05-04 14:58:43 +0200

    Map component refactor

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,88 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import L from "leaflet";
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap} from 'react-leaflet';
 import "./Map.css"
 
-const tileUrl = 'https://tiles.tinyarmy.org/1/1/{z}/{x}/{y}.jpg';
-const tileSize = 256;
-const minZoon = 2;
-const maxZoom = 7;
-const center = [-241, 368];
+const TILE_URL = 'https://tiles.tinyarmy.org/1/1/{z}/{x}/{y}.jpg';
+const TILE_SIZE = 256;
+const ZOOM_MIN = 2;
+const ZOOM_MAX = 7;
+const ZOOM_INITIAL = 3;
+const CENTER = [-241, 368];
+const COORDS_MIN = [0,0];
+const COORDS_MAX = [81920, 114688];
+const BOUNDS_VISCOSITY = 1.0;
+
+const icon = L.icon({
+  iconUrl: process.env.PUBLIC_URL + "/images/marker-icon.png",
+  iconSize: [25, 25],
+  iconAnchor: [12.5, 12.5]
+});
 
 
 function showPanoramaClick(eventArgs) {
-    console.log("Show panorama click: ", eventArgs.target.options.id);
+  console.log("Show panorama click: ", eventArgs.target.options.id);
 }
 
 function unproject(map, location) {
-    return map.unproject(location, map.getMaxZoom());
+  return map.unproject(location, map.getMaxZoom());
 }
 
+function MapController() {
+  
+  const map = useMap();
+  const bounds = L.latLngBounds(unproject(map, COORDS_MIN), unproject(map, COORDS_MAX)); 
+  const factorX = bounds.getEast() / COORDS_MAX[0];
+  const factorY = bounds.getSouth() / COORDS_MAX[1];
 
-function Markers() {
-    const map = useMap();
-    const bounds = L.latLngBounds(unproject(map, [0,0]), unproject(map, [81920, 114688]))
-
-	const factorX = bounds.getEast() / 81920;
-	const factorY = bounds.getSouth() / 114688;
-
-    const icon = L.icon({
-        iconUrl: process.env.PUBLIC_URL + "/images/marker-icon.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41]
-     });
-
+  function loadMarkers() {
     fetch("https://panoramas.raidcore.gg/data.json")
     .then((response) => response.json())
     .then((json) => json.forEach(element => {
+        console.log("Loading " + element.id)
         const marker = new L.Marker([element.y * factorY, element.x * factorX], {icon: icon});
-        marker.addTo(map);
+        marker.bindTooltip(element.id).addTo(map);
         marker.on('click', showPanoramaClick, this).options.id = element.id;
     }));
 
-    return null;
-}
-
-
-function MapBounds() {
-    const map = useMap();
-    const bounds = L.latLngBounds(unproject(map, [0,0]), unproject(map, [81920, 114688]));
-    map.setMaxBounds(bounds);
-
-    console.log(map);
-
-	console.log("N: " + bounds.getNorth());
-	console.log("E: " + bounds.getEast());
-	console.log("S: " + bounds.getSouth());
-	console.log("W: " + bounds.getWest());
-	
-	const factorX = bounds.getEast() / 81920;
-	const factorY = bounds.getSouth() / 114688;
-	
-	console.log(factorX);
-	console.log(factorY);
-
-    return null;
   }
 
+  useEffect(() => {
+    map.fitBounds(bounds);
+    map.setMaxBounds(bounds);
+    loadMarkers();
+  });
+
+  return (
+    <TileLayer
+      url={TILE_URL}
+      tileSize={TILE_SIZE}
+      maxBoundsViscosity={BOUNDS_VISCOSITY}
+      noWrap={!0}
+      bounds={bounds}
+    />
+  );
+}
+
 function Map() {
+
   return (
     <MapContainer
         style={{ height: "100vh", width: "100%", margin: 0}} 
-        zoom={3}
-        center={center}
+        zoom={ZOOM_INITIAL}
+        center={CENTER}
         crs={L.CRS.Simple}
-        minZoom={minZoon}
-        maxZoom={maxZoom}
+        minZoom={ZOOM_MIN}
+        maxZoom={ZOOM_MAX}
         attributionControl= {!1}>
-      <MapBounds/>
-      <Markers/>
-      <TileLayer
-        url={tileUrl}
-        tileSize={tileSize}
-        maxBoundsViscosity={1}
-        noWrap={!0}
-      />
+      <MapController/>
     </MapContainer>
   );
 }
