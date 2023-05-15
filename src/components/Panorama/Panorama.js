1f4831f8ce722a0fb6acb420aa30407adc027e11
Sognus <sognus@sognus.cz>
2023-05-15 16:32:37 +0200

    Basic visualization without controls

--- src/components/Panorama/Panorama.js
+++ src/components/Panorama/Panorama.js
@@ -1,6 +1,55 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import "./Panorama.css"
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import * as THREE from 'three';
+import "./Panorama.css";
+
+const NAVIGATION_CLOSE = "/";
+
+const TEXTURE_LINK_TEMPLATE = "https://panoramas.raidcore.gg/views/{id}.jpg"
+const TEXTURE_LINK_PLACEHOLDER = "{id}";
+
+const CAMERA_PERSPECTIVE_FOV = 75;
+const CAMERA_PERSPECTIVE_NEAR = 0.01;
+const CAMERA_PERSPECTIVE_FAR = 1100;
+const CAMERA_PERSPECTIVE_POSITION = [0,0,0];
+
+const PROJECTION_GEOMETRY_ARGS = [25, 20, 10];
+
+function Sphere({id}) {
+  const texture = useLoader(THREE.TextureLoader, TEXTURE_LINK_TEMPLATE.replace(TEXTURE_LINK_PLACEHOLDER, id));
+  const sphereRef = useRef();
+
+  useFrame(() => {
+    // Rotate the sphere if desired
+    sphereRef.current.rotation.y += 0.001;
+  });
+
+  return (
+    <mesh ref={sphereRef}>
+      <sphereGeometry args={PROJECTION_GEOMETRY_ARGS} />
+      <meshBasicMaterial attach="material" map={texture} side={THREE.DoubleSide} />
+    </mesh>
+  );
+}
+
+function Scene({id}) {
+  
+  return (
+    <>
+      <perspectiveCamera
+        fov={CAMERA_PERSPECTIVE_FOV}
+        aspect={window.innerWidth / window.innerHeight}
+        near={CAMERA_PERSPECTIVE_NEAR}
+        far={CAMERA_PERSPECTIVE_FAR}
+        position={CAMERA_PERSPECTIVE_POSITION}
+      />
+      <Sphere id={id} />
+    </>
+  );
+}
+
+
 
 function Panorama(props) {
 
@@ -8,13 +57,16 @@ function Panorama(props) {
   const { id } = useParams();
   
   function close() {
-    navigate("/");
+    navigate(NAVIGATION_CLOSE);
   }
 
   return (
-    <div style={{margin: "10px"}}>
-      <p style={{display: "inline-block", marginRight: "10px"}}>Panorama {id}</p>
-      <button onClick={close}>Close</button>
+    <div id="panorama">
+        <div class="info">Panorama {id}</div>
+        <button class="close-button" onClick={close}></button>
+        <Canvas>
+          <Scene id={id}/>
+        </Canvas>
     </div>
   );
 }
